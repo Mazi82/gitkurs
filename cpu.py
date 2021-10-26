5aedb5425d9cb74744678e6f353b43f4193086d4
Mazi82 <jafari.mazdak@gmail.com>
2021-10-26 12:20:45 +0200

    Changed git text

--- cpu.py
+++ cpu.py
@@ -1,1 +1,2 @@
 komplettera
+git test
---
+++ pension.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+str_age = input("Ange din ålder: ")
+num_age = int(str_age)
+
+years_left_to_pension = 65 - num_age
+print("Du har {} år kvar till att lata dig".format(years_left_to_pension))
+git test
